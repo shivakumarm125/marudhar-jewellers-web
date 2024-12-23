e3a3e9cdbeac0e0309f9dd827b4133b3f887fe93
shivakumarm125 <shivakumarm.2501@gmail.com>
2024-12-23 23:39:51 +0000

    basic product screen

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,7 +1,26 @@
 import { useParams } from "react-router-dom";
+import "./ProductDetails.scss";
+
 const ProductDetails = () => {
   const { id } = useParams();
-  return <>Product: {id}</>;
+  return (
+    <>
+      <div>Product: {id}</div>
+      <div className="product">
+        <div className="card__image">
+        <img
+            className="card__image--responsive"
+            src={'https://images.unsplash.com/photo-1417325384643-aac51acc9e5d'}
+            alt="Card description"
+          />
+        </div>
+        <div>
+          <div>Price : 200</div>
+          <div> how are you?</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductDetails;
